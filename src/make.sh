786b92efb08bd90e2f592824ab58e7fa85414c9b
utuhiro78 <34818411+utuhiro78@users.noreply.github.com>
2024-01-18 14:46:44 +0900

    Remove sudachidict-*.txt first

--- src/make.sh
+++ src/make.sh
@@ -3,6 +3,7 @@
 # Author: UTUMI Hirosi (utuhiro78 at yahoo dot co dot jp)
 # License: Apache License, Version 2.0
 
+rm sudachidict-*.txt
 ruby convert_sudachidict_to_mozcdic.rb
 ruby adjust_entries.rb mozcdic-ut-sudachidict.txt
 ruby filter_unsuitable_words.rb mozcdic-ut-sudachidict.txt
