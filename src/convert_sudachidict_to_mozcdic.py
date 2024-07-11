e06f6dda9806707d26660f1d348ded28133a3e4e
utuhiro78 <34818411+utuhiro78@users.noreply.github.com>
2024-07-12 01:36:47 +0900

    Fix styles

--- src/convert_sudachidict_to_mozcdic.py
+++ src/convert_sudachidict_to_mozcdic.py
@@ -35,7 +35,8 @@ dicname = "mozcdic-ut-sudachidict.txt"
 url = "https://raw.githubusercontent.com/google/mozc/master/src/data/dictionary_oss/id.def"
 with urllib.request.urlopen(url) as response:
 	id_mozc = response.read().decode()
-	id_mozc = id_mozc.split(" 名詞,一般,")[0].split("\n")[-1]
+
+id_mozc = id_mozc.split(" 名詞,一般,")[0].split("\n")[-1]
 
 l2 = []
 
